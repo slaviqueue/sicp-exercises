464f889ee2ea5c3d063f8b7a7894fc1c1713f8c1
Slava <electricsemyon@gmail.com>
2021-09-13 00:39:21 +0300

    Create 1.4.scm

---
+++ 1/1.4.scm
@@ -0,0 +1,8 @@
+; Exercise 1.4.
+; Observe that our model of evaluation allows for combinations whose operators are compound expressions. Use this observation to describe the behavior of the following procedure:
+
+(define (a-plus-abs-b a b)
+  ((if (> b 0) + -) a b))
+
+; This works because "+" and "-" are both regular functions, which are expressions.
+; In example above we are choosing which expression to apply to "a" and "b" operands.
