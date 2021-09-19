7406f4817c806b9329f461841281e0c7b0918c01
slaviqueue <electricsemyon@gmail.com>
2021-09-19 23:25:20 +0300

    solved 1.12

---
+++ chapter-1/1.11.rkt
@@ -0,0 +1,12 @@
+#lang racket
+
+; Exercise 1.12
+; Write a procedure that computes elements of
+; Pascal’s triangle by means of a recursive process.
+
+(define (pascal n)
+  (if (= n 0)
+    1
+    (+ (pascal (- n 1)) (pascal (- n 1)))))
+
+(pascal 20) ; 1048576
