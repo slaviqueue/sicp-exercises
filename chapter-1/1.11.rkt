33c19baf4664f0e9f4c2584326d54b3d542353f6
slaviqueue <electricsemyon@gmail.com>
2021-09-26 19:55:07 +0300

    solved 1.11 iterative

--- chapter-1/1.11.rkt
+++ chapter-1/1.11.rkt
@@ -1,12 +1,30 @@
 #lang racket
 
-(define (f n)
+(define (f-recursive n)
   (if (< n 3)
     n
     (+
-      (f (- n 1))
-      (* 2 (f (- n 2)))
-      (* 3 (f (- n 3))))))
+      (f-recursive (- n 1))
+      (* 2 (f-recursive (- n 2)))
+      (* 3 (f-recursive (- n 3))))))
 
-(f 3)  ; 3
-(f 10) ; 1892
+(define (f-iterative n)
+  (define (inc x) (+ x 1))
+
+  (define (iter i a b c)
+    (if (> i n)
+      c
+      (iter
+        (inc i)
+        b
+        c
+        (+ c (* b 2) (* a 3)))))
+
+  (if (< n 3) n (iter 3 0 1 2)))
+
+(f-recursive 3)  ; 3
+(f-recursive 10) ; 1892
+
+(f-iterative 3)  ; 3
+(f-iterative 10) ; 1892
+
