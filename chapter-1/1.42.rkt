f3a67794968b081932f695598ce48da2654b9ed8
slaviqueue <electricsemyon@gmail.com>
2021-10-14 14:21:24 +0300

    solved 1.42

---
+++ chapter-1/1.42.rkt
@@ -0,0 +1,16 @@
+#lang racket
+
+; Exercise 1.42
+
+; Let f and g be two one-argument functions.
+; The composition f after g is defined to be the function x 7→ f(g(x)).
+; Define a procedure compose that implements composition.
+
+(define (compose f g)
+  (lambda (x) (f (g x))))
+
+; Test:
+(define (square n) (* n n))
+(define (inc n) (+ n 1))
+
+((compose square inc) 6) ; 49
