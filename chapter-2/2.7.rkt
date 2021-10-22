49428d41ea670979b8f8484679eaf61057de3424
slaviqueue <electricsemyon@gmail.com>
2021-10-22 16:00:50 +0300

    solved 2.7

---
+++ chapter-2/2.7.rkt
@@ -0,0 +1,16 @@
+#lang racket
+
+; Exercise 2.7:
+
+; Alyssa’s program is incomplete because she has not specified the implementation of the
+; interval abstraction. Here is a definition of the interval constructor:
+(define (make-interval a b) (cons a b))
+
+; Define selectors upper-bound and lower-bound to complete the implementation.
+
+(define (upper-bound interval)
+  (car interval))
+
+(define (lower-bound interval)
+  (cdr interval))
+
