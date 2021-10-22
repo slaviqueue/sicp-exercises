f2fe8b170a6159123187748be55a89b280f0a9ac
slaviqueue <electricsemyon@gmail.com>
2021-10-22 16:48:18 +0300

    solved 2.17

---
+++ chapter-2/2.17.rkt
@@ -0,0 +1,16 @@
+#lang racket
+
+; Exercise 2.17:
+
+; Define a procedure last-pair that returns the list that contains only the last
+; element of a given (nonempty) list:
+
+; (last-pair (list 23 72 149 34))
+; (34)
+
+(define (last-pair list)
+  (if (null? (cdr list))
+      (list (car list))
+      (last-pair (cdr list))))
+
+(last-pair (list 23 72 149 34)) ; (34)
